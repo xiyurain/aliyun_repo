24268bec33377d5f1ba106283a13458f53e9d28f
xiyurain <42943762+xiyurain@users.noreply.github.com>
2019-11-30 14:20:09 +0800

    Add files via upload

--- socket_demo.py
+++ socket_demo.py
@@ -23,34 +23,25 @@ work = True
 while(work):
     command = str(sock_conn.recv(1024),'utf-8')
     if command == 'signup':
-        print('sign up.')
         #register
         sock_conn.sendall(bytes('triggered_signup',encoding='utf-8'))
         #reg info
         user_name = str(sock_conn.recv(1024),'utf-8')
-        print('get username.')
         sock_conn.sendall(bytes('got_username',encoding='utf-8'))
         password = str(sock_conn.recv(1024),'utf-8')
-        print('get password.')
         sock_conn.sendall(bytes('got_password',encoding='utf-8'))
         ip_address = str(sock_conn.recv(1024),'utf-8')
-        print('get ip.')
         sock_conn.sendall(bytes('got_ip',encoding='utf-8'))
-        print('search.')
         sql = ("SELECT * FROM login_info WHERE user_name='%s'" %(user_name))
         cursor.execute(sql)
         if(cursor.rowcount):
             sock_conn.sendall(bytes('duplicated',encoding='utf-8'))
             continue
         else:
-            print('insert.')
             sql = ("INSERT INTO login_info(user_name,password,ip_address)VALUES('%s','%s','%s')" % (user_name,password,ip_address))
             #insert
             try:
-                cursor.execute(sql)
-                print('execute.')
                 db_conn.commit()
-                print('commit.')
             except:
                 db_conn.rollback()
             sock_conn.sendall(bytes('stored',encoding='utf-8'))
@@ -60,8 +51,11 @@ while(work):
         sock_conn.sendall(bytes('triggered_login',encoding='utf-8'))
         #log_info
         user_name = str(sock_conn.recv(1024),'utf-8')
+        sock_conn.sendall(bytes('got_username',encoding='utf-8'))
         password = str(sock_conn.recv(1024),'utf-8')
+        sock_conn.sendall(bytes('got_password',encoding='utf-8'))
         ip_address = str(sock_conn.recv(1024),'utf-8')
+        sock_conn.sendall(bytes('got_ip',encoding='utf-8'))
         
         sql = ("SELECT * FROM login_info WHERE user_name='%s' AND password='%s'" %(user_name,password))
         cursor.execute(sql)
