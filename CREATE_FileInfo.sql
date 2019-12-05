5affd408df8693930ff801445625c1f667389d5b
xiyurain <42943762+xiyurain@users.noreply.github.com>
2019-12-05 23:14:09 +0800

    Add files via upload

--- CREATE_FileInfo.sql
+++ CREATE_FileInfo.sql
@@ -3,8 +3,8 @@ CREATE TABLE file_info
 (
 	file_id 	int			NOT NULL AUTO_INCREMENT,
     file_name	char(100)	NOT NULL,
-    file_size	float		NULL,
+    file_size	char(20)	NULL,
     uploader	char(50)	NOT NULL,
-    sharetime	char(50)	NOT NULL,
+    sharetime	char(50)	NULL,
     PRIMARY KEY	(file_id)
 )ENGINE=InnoDB;
